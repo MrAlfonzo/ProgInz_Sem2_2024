54b0145c29d6fcda8f0adf597a6e1171aadaf855
MrAlfonzo <s23mactalfr@venta.lv>
2024-05-10 12:09:06 +0300

    Add: added Course class with lombok, validation and jpa

--- src/main/java/lv/venta/model/Course.java
+++ src/main/java/lv/venta/model/Course.java
@@ -1,5 +1,47 @@
 package lv.venta.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+//@AllArgsConstructor
+@Table(name = "CourseTable")
+@Entity
 public class Course {
 
+	@Setter(value = AccessLevel.NONE)
+	@Column(name = "Idc")
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO) // autoincrement
+	private int idc;
+	
+	@NotNull
+	@Size(min = 4, max = 50)
+	@Pattern(regexp = "[A-ZĒŪĪĻĶĢŠĀŽČŅa-zēūīļķģšāžčņ ]+", message = "Only letters and space are allowed")
+	@Column(name = "Title")
+	private String title;
+	
+	@Column(name = "Creditpoints")
+	@Min(1)
+	@Max(20)
+	private int creditpoints;
+	
+	//TODO uztaisit konst, pec saistes pievienoshanu ar profesoru
 }
